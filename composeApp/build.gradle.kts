4b0823b7da1cd45b874e920611634da4dcad4840
sakethpathike <sakethpathike@gmail.com>
2026-04-07 17:55:56 +0530

    chore: bump app version to 0.17.0 and update F-Droid metadata

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -144,8 +144,8 @@ android {
         applicationId = "com.sakethh.linkora"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 48
-        versionName = "0.16.0"
+        versionCode = 49
+        versionName = "0.17.0"
     }
     packaging {
         resources {
@@ -212,7 +212,7 @@ compose.desktop {
             )
             packageName = "Linkora"
             this.vendor = "Saketh Pathike"
-            this.packageVersion = "1.0.14"/*
+            this.packageVersion = "1.0.15"/*
 
             This logo (src/desktopMain/resources/logo.*) was painted by `mondstern`.
             The original post can be found here: https://pixelfed.social/p/mondstern/747494483548287527
--- composeApp/src/commonMain/kotlin/com/sakethh/linkora/utils/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/sakethh/linkora/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.sakethh.linkora.utils
 
 object Constants {
-    const val APP_VERSION_NAME = "v0.16.0"
+    const val APP_VERSION_NAME = "v0.17.0"
     const val LOCALIZATION_SERVER_URL = "https://linkoralocalizationserver.onrender.com/"
     const val PROXY_SERVER_URL = "https://linkora-proxy.onrender.com"
 
