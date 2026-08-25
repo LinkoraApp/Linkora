4784408fd47fd1a05619dd99fddd4240b432dc31
sakethpathike <sakethpathike@gmail.com>
2026-08-25 13:46:47 +0530

    chore(deps): bump dependencies
    - Room: 3.0.1
    - AGP: 8.13.2

--- composeApp/src/commonMain/kotlin/com/sakethh/linkora/data/local/TypeConverter.kt
+++ composeApp/src/commonMain/kotlin/com/sakethh/linkora/data/local/TypeConverter.kt
@@ -1,21 +1,27 @@
 package com.sakethh.linkora.data.local
 
-import androidx.room3.TypeConverter
+import androidx.room3.ColumnTypeConverter
 import com.sakethh.linkora.domain.LinkType
 import com.sakethh.linkora.domain.MediaType
 import com.sakethh.linkora.domain.model.tag.LinkTag
 import kotlinx.serialization.json.Json
 
 class TypeConverter {
-    @TypeConverter fun stringToLinkType(string: String): LinkType = LinkType.valueOf(string)
+    @ColumnTypeConverter
+    fun stringToLinkType(string: String): LinkType = LinkType.valueOf(string)
 
-    @TypeConverter fun linkTypeToString(linkType: LinkType): String = linkType.name
+    @ColumnTypeConverter
+    fun linkTypeToString(linkType: LinkType): String = linkType.name
 
-    @TypeConverter fun stringToMediaType(string: String): MediaType = MediaType.valueOf(string)
+    @ColumnTypeConverter
+    fun stringToMediaType(string: String): MediaType = MediaType.valueOf(string)
 
-    @TypeConverter fun mediaTypeToString(mediaType: MediaType): String = mediaType.name
+    @ColumnTypeConverter
+    fun mediaTypeToString(mediaType: MediaType): String = mediaType.name
 
-    @TypeConverter fun fromTagList(value: List<LinkTag>): String = Json.encodeToString(value)
+    @ColumnTypeConverter
+    fun fromTagList(value: List<LinkTag>): String = Json.encodeToString(value)
 
-    @TypeConverter fun toTagList(value: String): List<LinkTag> = Json.decodeFromString(value)
+    @ColumnTypeConverter
+    fun toTagList(value: String): List<LinkTag> = Json.decodeFromString(value)
 }
